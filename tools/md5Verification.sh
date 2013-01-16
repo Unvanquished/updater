e6fd184e4ab908d250accbe59a1ecb76c25b04ea
Daniel Maloney <daniel.r.maloney@gmail.com>
2013-01-17 02:22:09 +1100

    Add MD5 verification under OS X
    OS X does not come with the standard md5sum tool so we have to use a
    script to compare the message digest.

---
+++ tools/md5Verification.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Intended to be used on OS X as it does not ship with md5sum
+badAssets=();
+
+while IFS=" *" read assetMD5 assetName; do
+	MD5Output=($( md5 -r $assetName 2>&1 ))
+	if [[ ${MD5Output[0]} != $assetMD5 && ${MD5Output[@]} != *'No such file or directory'  ]]; then
+		badAssets+=($assetName);
+	fi;
+done < md5sums0.11;
+
+for asset in "${badAssets[@]}"; do
+	echo "$asset";
+done;
+
