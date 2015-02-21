277834efba4dd1c9edd30d17439a6beafa115ac0
dolcetriade <vcelestialragev@gmail.com>
2015-02-20 16:57:01 -0800

    Load font from resources. Use qt.conf to set paths

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    int fontId = QFontDatabase::addApplicationFont(":fonts/Roboto-Regular.ttf");
+    if (fontId != -1) {
+        QFont font("Roboto-Regular");
+        font.setPointSize(10);
+        a.setFont(font);
+    }
+
     w.setWindowTitle("Unvanquished Updater");
     w.show();
 
