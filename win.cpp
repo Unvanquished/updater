a6f6b27b84fd8d86f965880c70fb74b06bc0f45f
dolcetriade <vcelestialragev@gmail.com>
2017-06-11 17:31:32 -0700

    Add initial support for creating Windows start menu shortcuts.

--- win.cpp
+++ win.cpp
@@ -1,7 +1,17 @@
-#include <windows.h>
+#define WINVER 0x0600
+#define _WIN32_WINNT 0x0600
+#include "shlobj.h"
+#include "windows.h"
+#include "winnls.h"
+#include "shobjidl.h"
+#include "objbase.h"
+#include "objidl.h"
+#include "shlguid.h"
+
 #include "system.h"
 #include "settings.h"
 #include <QSettings>
+#include <QDir>
 
 
 namespace {
@@ -11,6 +21,41 @@ void setRegistryKey(const QString& key,
 {
     QSettings registry(key, QSettings::NativeFormat);
     registry.setValue(name, value);
+}
+
+// Create a Shortcut. Code from https://msdn.microsoft.com/en-us/library/bb776891(VS.85).aspx
+bool CreateLink(const QString& sourcePath, const QString& workingDir,
+                const QString& linkPath, QString& linkName)
+{
+    HRESULT hres;
+    IShellLink* psl;
+
+
+    // Get a pointer to the IShellLink interface. It is assumed that CoInitialize
+    // has already been called.
+    hres = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, IID_IShellLink, (LPVOID*)&psl);
+    if (SUCCEEDED(hres))
+    {
+        IPersistFile* ppf;
+
+        // Set the path to the shortcut target and add the description.
+        psl->SetPath(sourcePath.toStdWString().c_str());
+        psl->SetWorkingDirectory(workingDir.toStdWString().c_str());
+        psl->SetDescription(linkName.toStdWString().c_str());
+
+        // Query IShellLink for the IPersistFile interface, used for saving the
+        // shortcut in persistent storage.
+        hres = psl->QueryInterface(IID_IPersistFile, (LPVOID*)&ppf);
+
+        if (SUCCEEDED(hres))
+        {
+            // Save the link by calling IPersistFile::Save.
+            hres = ppf->Save(linkPath.toStdWString().c_str(), TRUE);
+            ppf->Release();
+        }
+        psl->Release();
+    }
+    return SUCCEEDED(hres);
 }
 }  // namespace
 namespace Sys {
@@ -64,6 +109,8 @@ bool install(void)
 {
     Settings settings;
     QString installPath = settings.installPath();
+
+    // Create unv:// protocol handler
     setRegistryKey("HKEY_CLASSES_ROOT\\unv", "Default", "URL: Unvanquished Protocol");
     setRegistryKey("HKEY_CLASSES_ROOT\\unv\\DefaultIcon", "Default",
                    installPath + "\\daemon.exe,1");
@@ -71,6 +118,26 @@ bool install(void)
     setRegistryKey("HKEY_CLASSES_ROOT\\unv\\shell\\open\\command", "Default",
                    installPath + "\\daemon.exe -pakpath " + installPath +
                         " +connect \"%1\"");
+
+    // Create a start menu shortcut
+    // By default, install it to the users's start menu, unless they are instaling
+    // the game globally.
+    auto startMenuTye = FOLDERID_Programs;
+    if (installPath.contains("Program Files", Qt::CaseInsensitive)) {
+        startMenuTye = FOLDERID_CommonPrograms;
+    }
+    PWSTR path = nullptr;
+    auto ret = SHGetKnownFolderPath(startMenuTye, 0, nullptr, &path);
+    // TODO: Bubble up error if this call fails.
+    if (!SUCCEEDED(ret)) {
+        CoTaskMemFree(path);
+        return true;
+    }
+    QDir dir(QString::fromStdWString(std::wstring(path)));
+    dir.mkdir("Unvanquished");
+    dir.setPath(dir.path() + "\\Unvanquished");
+    QString linkName = "Unvanquished";
+    CreateLink(installPath + "\\daemon.exe", installPath, dir.path() + "\\Unvanquished.lnk", linkName);
     return true;
 }
 
