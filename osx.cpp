d39e78eb17fa4399eb412cde9585b303ece4b232
DolceTriade <vcelestialragev@gmail.com>
2017-09-10 15:42:30 -0700

    Add support for OSX autoupdate.

--- osx.cpp
+++ osx.cpp
@@ -6,6 +6,29 @@
 #include <QFile>
 #include <QDebug>
 #include <QCoreApplication>
+#include <QFileInfo>
+#include <QProcess>
+
+namespace {
+// Enter path of file within an app, and get the
+// path of the app. Returns empty string if not within
+// an app.
+QString extractAppPath(const QString& path) {
+    QFileInfo info(path);
+    if (info.canonicalFilePath().contains(".app")) {
+        QDir path = info.absoluteDir();
+        while (!path.isRoot() && !path.absolutePath().endsWith(".app")) {
+            path.cdUp();
+        }
+        if (path.isRoot()) {
+            qDebug() << "Failed to find .app name.";
+            return "false""";
+        }
+        return path.absolutePath();
+    }
+    return "";
+}
+}
 
 namespace Sys {
 QString archiveName(void)
@@ -33,36 +56,56 @@ bool install(void)
 
 bool updateUpdater(const QString& updaterArchive)
 {
-    QDir dir(QCoreApplication::applicationFilePath());
-    if (!(dir.cdUp() && dir.cdUp() && dir.cdUp())) {
-        qDebug() << "Error executing cdUp()!";
-        return false;
-    }
-    QString name = dir.dirName();
-    if (!name.endsWith(".app")) {
-        qDebug() << "Expected app to be started from an OSX .app";
-        return false;
-    }
-    if (!dir.cdUp()) {
-        qDebug() << "Unexpected failure...";
-        return false;
-    }
-    QDir oldUpdate(dir.absolutePath() + "/" + name + ".bak");
-    if (oldUpdate.exists()) {
-        if (!oldUpdate.removeRecursively()) {
+    QString currentAppPath = extractAppPath(QCoreApplication::applicationFilePath());
+    if (currentAppPath.isEmpty()) return false;
+    QDir currentApp(currentAppPath);
+    QDir backupUpdater(currentAppPath +  ".bak");
+    if (backupUpdater.exists()) {
+        if (!backupUpdater.removeRecursively()) {
             qDebug() << "Error deleting old update. pls update manually";
             return false;
         }
     }
-    if (!dir.rename(name, oldUpdate.dirName())) {
+    QDir extractDir = backupUpdater;
+    extractDir.cdUp();
+    if (!extractDir.rename(currentApp.dirName(), backupUpdater.dirName())) {
+        qDebug() << "Could not rename bkup. pls manually update.";
+        return false;
+    }
+    QStringList extractedFiles = JlCompress::extractDir(updaterArchive, extractDir.absolutePath());
+    if (extractedFiles.size() < 1) {
+        return false;
+    }
+    // Extract the .app name.
+    // Since it's an .app file, it must contain an Info.plist. Use
+    // this file to determine the .app name in the zip.
+    // NOTE: We could have hard coded this, but there is no guarantee
+    // that the person running this app is running the app that has
+    // the same name as the one in the zip.
+    constexpr char kFileInApp[] = "Info.plist";
+    QStringList infos = extractedFiles.filter(kFileInApp);
+    if (infos.size() < 1) {
+        qDebug() << "Error finding " << kFileInApp;
+        return false;
+    }
+
+    QString extractedAppPath = "";
+    for (int i = 0; i < infos.size() && extractedAppPath.isEmpty(); ++i) {
+        extractedAppPath = extractAppPath(infos[i]);
+    }
+    if (extractedAppPath.isEmpty()) return false;
+    QDir extractedApp(extractedAppPath);
+    if (!extractDir.rename(extractedApp.dirName(), currentApp.dirName())) {
         qDebug() << "Could not rename update. pls manually update.";
         return false;
     }
-    qDebug() << "current=" << dir.absolutePath() << " update=" << update;
-    auto out = JlCompress::extractDir(updaterArchive, dir.absolutePath());
-    if (out.size() < 1) {
+    // TODO: Maybe don't use system? startDetached didn't work for me...
+    int i = system((QString("/usr/bin/open -F -n \"%1\"").arg(currentAppPath)).toStdString().c_str());
+    if (i != 0) {
+        qDebug() << "Error launching new updater. Please try manually.";
         return false;
     }
+    QCoreApplication::quit();
     return true;
 }
 
