e37fab1ef43ff0c1b8b20083db9d440c07074e9a
slipher <slipher@protonmail.com>
2020-12-31 02:37:23 -0600

    Ensure C++ objects are destructed after QML engine
    This prevents several errors like
    "TypeError: Cannot read property 'completedSize' of null" from
    appearing whenever the application is closed.
    
    The occurrence of this error suggests that the QML engine is still running
    QML code in its destructor, after app.exec() returns, which seems
    stupid...

--- main.cpp
+++ main.cpp
@@ -77,13 +77,13 @@ int main(int argc, char *argv[])
         font.setPointSize(10);
         app.setFont(font);
     }
+    Settings settings;
+    QmlDownloader downloader;
     QQmlApplicationEngine engine;
     engine.addImportPath(QLatin1String("qrc:/"));
     engine.addImageProvider(QLatin1String("fluidicons"), new IconsImageProvider());
     engine.addImageProvider(QLatin1String("fluidicontheme"), new IconThemeImageProvider());
     auto* context = engine.rootContext();
-    Settings settings;
-    QmlDownloader downloader;
     context->setContextProperty("updaterSettings", &settings);
     context->setContextProperty("downloader", &downloader);
     qmlRegisterType<QmlDownloader>("QmlDownloader", 1, 0, "QmlDownloader");
