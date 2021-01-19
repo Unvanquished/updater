ca66b586f13cb0cf729e4753afed51bbb641d16e
slipher <slipher@protonmail.com>
2021-01-19 04:37:28 -0600

    Remove useless installFinished setting

--- settings.cpp
+++ settings.cpp
@@ -24,15 +24,7 @@ QString Settings::currentVersion() const {
     return settings_->value(Settings::CURRENT_VERSION).toString();
 }
 
-bool Settings::installFinished() const {
-    return settings_->value(Settings::INSTALL_FINISHED, false).toBool();
-}
-
 void Settings::setInstallPath(const QString& installPath) {
-    // If we changed the download path, make sure we don't start the game.
-    if (installPath != this->installPath()) {
-        setInstallFinished(false);
-    }
     settings_->setValue(Settings::INSTALL_PATH, installPath);
     emit installPathChanged(installPath);
 }
@@ -46,10 +38,3 @@ void Settings::setCurrentVersion(const QString& currentVersion) {
     settings_->setValue(Settings::CURRENT_VERSION, currentVersion);
     emit currentVersionChanged(currentVersion);
 }
-
-void Settings::setInstallFinished(bool installFinished) {
-    settings_->setValue(Settings::INSTALL_FINISHED, installFinished);
-    emit installFinishedChanged(installFinished);
-}
-
-
