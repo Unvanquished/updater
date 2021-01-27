17a107b2f44ba685fa60fcb3befbf886973b6c0f
slipher <slipher@protonmail.com>
2021-01-27 04:51:45 -0600

    Azure Pipelines config to build Mac updater
    This is the version I used to build the v0.1.0 release.

--- mac-build-aria.sh
+++ mac-build-aria.sh
@@ -4,7 +4,7 @@ cd aria2
 git clean -dXff
 export MACOSX_DEPLOYMENT_TARGET=10.13  # To match Qt's target
 AUTOPOINT=$(brew --prefix gettext)/bin/autopoint autoreconf -i
-./configure --without-libxml2 --without-libexpat --without-sqlite3 --enable-libaria2 --without-zlib --without-libcares --enable-static=yes ARIA2_STATIC=yes --without-libssh2 --disable-websocket --disable-nls
+./configure --without-libxml2 --without-libexpat --without-sqlite3 --enable-libaria2 --without-zlib --without-libcares --without-libgmp --without-libnettle --enable-static=yes ARIA2_STATIC=yes --without-libssh2 --disable-websocket --disable-nls
 # Build only src/ to skip the gettext infrastructure targets which complain about
 # gettext being newer than the version required in configure.ac
 cd src
