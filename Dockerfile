ea0f8cc2c56d9c851365c5dad26d2e1bfb1055a3
slipher <slipher@protonmail.com>
2025-11-03 04:37:09 -0600

    Always use a shell script to build aria2
    Finally take care of those unwieldily duplicated argument lists for
    aria2 configure by using a single shell script. I just got bit by Nettle
    not being disabled in one of the cases. Also I noticed that
    --without-libz was sometimes misspelled as --without-zlib.
    
    test

--- Dockerfile
+++ Dockerfile
@@ -58,9 +58,9 @@ RUN curl -LO https://download.qt.io/archive/qt/5.14/5.14.2/single/qt-everywhere-
 ###############
 COPY aria2 /updater/aria2
 COPY .git/modules/aria2 /updater/.git/modules/aria2
+COPY build-aria.sh /updater/
 WORKDIR /updater/aria2
-RUN git clean -dXff
-RUN autoreconf -i && OPENSSL_LIBS='-L/openssl/lib64 -lssl -lcrypto -lpthread -ldl' OPENSSL_CFLAGS='-I /openssl/include' ./configure --without-libxml2 --without-libexpat --without-sqlite3 --enable-libaria2 --without-zlib --without-libcares --enable-static --disable-shared --without-libssh2 --disable-metalink --disable-websocket --disable-nls --with-openssl && make -j`nproc`
+RUN OPENSSL_LIBS='-L/openssl/lib64 -lssl -lcrypto -lpthread -ldl' OPENSSL_CFLAGS='-I /openssl/include' ../build-aria.sh --with-openssl
 
 #################
 # Build updater #
---
+++ build-aria.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Any command line arguments will be passed to aria2 configure.
+
+set -e
+
+if [ ! -f src/aria2api.cc ]; then
+    echo 'Error: must build from root of aria2 tree'
+    exit 1
+fi
+
+case "$(uname)" in
+	Darwin*)
+        export MACOSX_DEPLOYMENT_TARGET=10.13  # To match Qt's target
+        export AUTOPOINT=$(brew --prefix gettext)/bin/autopoint
+    ;;
+esac
+
+num_processors=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 2)
+
+set -x # echo commands
+
+git clean -dXff
+autoreconf -i
+
+# For crypto/TLS we use OpenSSL on Linux and the OS-provided one on others.
+./configure \
+    --enable-static --disable-shared \
+    --enable-libaria2 \
+    --disable-metalink --disable-websocket --disable-nls \
+    --without-libnettle --without-libgmp --without-libgcrypt --without-gnutls \
+    --without-sqlite3 --without-libxml2 --without-libexpat --without-libcares --without-libz --without-libssh2 \
+    "$@"
+
+# Build only src/ to skip the gettext infrastructure targets which, at some point on Mac, complained about
+# gettext being newer than the version required in configure.ac
+cd src
+MAKEFLAGS="-j${num_processors} ${MAKEFLAGS}" make
